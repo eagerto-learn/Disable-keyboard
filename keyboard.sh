ffae596c421dfe076985629fdcb33fa7d0b8d3c4
eagerto-learn <51919789+eagerto-learn@users.noreply.github.com>
2021-03-15 17:26:29 +0100

    Update keyboard.sh

--- keyboard.sh
+++ keyboard.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-fconfig="/home/user/Scripts/.keyboard"
-echo "disabled" > $fconfig
-
 xinput list | grep AT | grep -o 'id=[0-9]*' | cut -c 4- > ~/Scripts/.keyboardcheck
 keyboardcheck="/home/user/Scripts/.keyboardcheck"
 id=$(cat "$keyboardcheck")
