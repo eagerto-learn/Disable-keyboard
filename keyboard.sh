034fc67f2b1f0a0cd25798ed07bcba959f312f36
eagerto-learn <51919789+eagerto-learn@users.noreply.github.com>
2023-03-14 17:38:34 +0100

    Update keyboard.sh

--- keyboard.sh
+++ keyboard.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-xinput list | grep AT | grep -o 'id=[0-9]*' | cut -c 4- > ~/Scripts/.keyboardcheck
-keyboardcheck="/home/user/Scripts/.keyboardcheck"
-id=$(cat "$keyboardcheck")
+id=$(xinput list | grep AT | grep -o 'id=[0-9]*' | cut -c 4-)
 
 xinput float $id
