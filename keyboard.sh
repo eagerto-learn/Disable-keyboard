3163352bb784c6f18d421090bd0cf3765515d855
eagerto-learn <51919789+eagerto-learn@users.noreply.github.com>
2023-05-07 23:51:31 +0200

    Update keyboard.sh

--- keyboard.sh
+++ keyboard.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 id=$(xinput list | grep AT | grep -o 'id=[0-9]*' | cut -c 4-)
+status=$(xinput list | grep AT | grep -o floating)
 
-xinput float $id
+if [ "$status" = "floating" ]
+	then 
+		notify-send "Enabling keyboard..." \ "ON - Keyboard connected!";
+		echo "enabling keyboard..."
+		xinput reattach $id 3
+		echo "enabled"
+	elif [ -z $status ]
+	then
+		notify-send "Disabling keyboard..." \ "OFF - Keyboard disconnected!";
+		echo "disabling keyboard..."
+		xinput float $id
+		echo "disabled"
+fi
